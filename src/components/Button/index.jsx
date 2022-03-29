fc31f6ced099da41184be94935b9b8dc0fb43a04
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2022-03-29 10:25:09 -0300

    create: init project ReactJS

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+// children é a palavra reservada para acessar o conteúdo
+const ButtonComp = ({children}) => {
+  return (
+    <>
+      <button className="button-1">{children}</button>
+
+      <h2>Texto com o botão</h2>
+    </>
+  );
+};
+
+export default ButtonComp;
