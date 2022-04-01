501c34628529d6b0e769cca86fd6026d4e1525e1
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2022-04-01 13:46:01 -0300

    update: Adicionando explicacao sobre return implicito e return explicito

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,22 @@
 // import "./styles.css";
+/*
+ * Quando não temos lógica no nosso componente e precisamos reotrnar só o JSX, podemos usar a Arrow Function com retorno implícito ()=>() (como estamos usando no componente atual). */
+/*const Button = ({ onClick, styleButton, children }) => (
+  <button onClick={onClick} className={styleButton}>
+    {children}
+  </button>
+);*/
+
+/*
+ * Quando precisamos aplicar alguma lógica no componente precisamos usar chaves no escopo da Arrow Function com c ()=>{Return()})
+ */
+
 const Button = ({ onClick, styleButton, children }) => {
+  // Com o return explícito aqui vai a lógica :)
   return (
     <button onClick={onClick} className={styleButton}>
-      {children}
+      {" "}
+      {children}{" "}
     </button>
   );
 };
