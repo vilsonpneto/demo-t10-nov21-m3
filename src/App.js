7ba1ad3c9dc06608b4d01afc53189d8fb7347381
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2022-04-04 18:38:24 -0300

    update: adicionando comentarios explicativos e requisicao API com async await

--- src/App.js
+++ src/App.js
@@ -6,12 +6,36 @@ import Characters from "./components/Characters";
 function App() {
   const [dataApi, setDataApi] = useState([]);
 
+  //Exemplo Hook useEffect() fazendo requisição em API com fetch.then
   useEffect(() => {
+    // fazendo uma busca no endereço da API
     fetch("https://www.swapi.tech/api/people")
+      // Quando houver a resposta, converter os dados para JSON
       .then((response) => response.json())
+      // Usando os dados convertidos
       //.then((data) => console.log(data));
       .then((data) => setDataApi(data));
   }, []);
+
+  /*Exemplo Hook useEffect() fazendo requisição em API com async await, mas para fazer o map preciso 
+  fazer essa atribuição const newResponseResults = newResponse.results, e para usar a URL da próxima página precisaria
+  mudar a lógica do meu sistema.
+  */
+  /*useEffect(() => {
+    async function fetchCharacters() {
+      const response = await fetch("https://www.swapi.tech/api/people");
+
+      const newResponse = await response.json();
+      const newResponseResults = newResponse.results
+      console.log(newResponseResults);
+
+      const newCharacters = newResponseResults.map((character) => ({
+        ...character,
+      }));
+      setDataApi(newCharacters);
+    }
+    fetchCharacters();
+  }, []); */
 
   return (
     <div className="App">
